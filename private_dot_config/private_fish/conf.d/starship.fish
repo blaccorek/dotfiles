fe33655d0f8cbb210c6b96ab461ab4b53740fe35
blaccorek <jt.andriamihamina@gmail.com>
2025-09-05 15:50:33 +0200

    remove transient

--- private_dot_config/private_fish/conf.d/starship.fish
+++ private_dot_config/private_fish/conf.d/starship.fish
@@ -1,11 +1,7 @@
 # Starship init
 
 if which starship > /dev/null 2>&1
-    function starship_transient_rprompt_func
-        starship module time
-    end
     starship init fish | source
-    enable_transience
 else
     echo "Starship not found, please install it from https://starship.rs/"
 end
